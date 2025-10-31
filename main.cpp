36d8b4edfe3e86b37157890e33603841dbb7502b
vishalsharma-505 <vishalsharmadbz@gmail.com>
2025-10-31 07:40:54 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class Character {
+public:
+    string name;
+    int age;
+    string gender;
+    string race;
+    float height;
+
+    void inputDetails() {
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter age: ";
+        cin >> age;
+        cout << "Enter gender: ";
+        cin >> gender;
+        cout << "Enter race: ";
+        cin >> race;
+        cout << "Enter height (in meters): ";
+        cin >> height;
+    }
+
+    void printName() {
+        cout << "Name: " << name << endl;
+    }
+
+    void printAge() {
+        cout << "Age: " << age << endl;
+    }
+
+    void printRace() {
+        cout << "Gender: " << gender << endl;
+        cout << "Race: " << race << endl;
+    }
+
+    void printHeight() {
+        cout << "Height: " << height << " meters" << endl;
+    }
+
+    void printAttackPower() {
+        cout << "Attack Power: " << height * 10 << endl;
+    }
+};
+
+int main() {
+    Character c;
+    c.inputDetails();
+
+    cout << "\n--- Character Info ---\n";
+    c.printName();
+    c.printAge();
+    c.printRace();
+    c.printHeight();
+    c.printAttackPower();
+
+    return 0;
+}
